24df4b315ba7b2137082719cf09f7ed14892b9a5
Santiago Penas <penassantiago@gmail.com>
2024-06-06 20:13:41 -0300

    agregamos carpeta front y emepzamos conReact

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componentes/layout/Header'; 
+import Nav from './componentes/layout/Nav';
+import Footer from './componentes/layout/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ front/src/componentes/layout/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'; //etsa lina puede estar o no, por las dudas la
+
+const Header = () => {
+    return (
+        <div>Header</div>
+    );
+};
+
+export default Header;
