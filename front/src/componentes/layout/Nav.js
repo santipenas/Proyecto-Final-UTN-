379badb433538a6cc2f273fe35da9ee6ee10bcef
Santiago Penas <penassantiago@gmail.com>
2024-06-11 16:48:42 -0300

    navlink y clase Activo del nav

--- front/src/componentes/layout/Nav.js
+++ front/src/componentes/layout/Nav.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = (props) => {
     return (
             <nav className='holder'>  
                 <div> 
                     <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/nosotros'>Nosotros</Link></li>
-                        <li><Link to='/contacto'>Contacto</Link></li>
-                        <li><Link to='/galeria'>Galeria</Link></li>
-                        <li><Link to='/servicios'>Servicios</Link></li>
-                        <li><Link to='/novedades'>Novedades</Link></li>
+                        <li><NavLink to='/' className={({isActive}) => isActive ? 'activo' : undefined}>Home</NavLink></li> 
+                        <li><NavLink to='/nosotros' className={({isActive}) => isActive ? 'activo' : undefined}>Nosotros</NavLink></li>
+                        <li><NavLink to='/contacto' className={({isActive}) => isActive ? 'activo' : undefined}>Contacto</NavLink></li>
+                        <li><NavLink to='/galeria' className={({isActive}) => isActive ? 'activo' : undefined}>Galeria</NavLink></li>
+                        <li><NavLink to='/servicios' className={({isActive}) => isActive ? 'activo' : undefined}>Servicios</NavLink></li>
+                        <li><NavLink to='/novedades' className={({isActive}) => isActive ? 'activo' : undefined}>Novedades</NavLink></li>
                     </ul>
                 </div> 
             </nav>
