26b18cdef22e3c030d68781dce2d421f71b0bbaa
Santiago Penas <penassantiago@gmail.com>
2024-07-28 17:33:27 -0300

    agregue e integre BD inmobiliaria y funciona ok

--- back/app.js
+++ back/app.js
@@ -4,12 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//la linea de abajo es para indicar que el proyecto va atener variable sde entorno tipo dotenv, par ala integracion con la BD
+require('dotenv').config();
+
+
+//armando variables de sesion, luego vere si debo usarlas en el proyecto final. Aca activo en el renglnde abajo que sea requisito las varaibles de sesion
+// var session = require('express-session');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //empeizo a armar la ruta del adimn y login
 var loginRouter = require('./routes/admin/login');
 
-//armando variables de sesion, luego vere si debo usarlas en el proyecto final
+
 
 var app = express();
 
@@ -23,13 +31,61 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* POR AHORA DEJARLO COMENTADO, AL HABILITARLAS ME HABIA DADO ERROR 
+//variables sesion, esto es de la tarea de petiiciones y respuesta, Handlebars y va junto con el archivo index.hbs
+
+app.use(session({
+  secret: 'santiago',
+  resave: false,
+  saveUninitialized: true
+}));*/
+
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// abajo conecto con la BD para hacer las consutlas
+var pool = require('./models/bd');
+
+// consulta de SELECT (ANDA PERFECTO)
+pool.query('select * from inmobiliaria').then(function(resultados) {
+  console.log(resultados);
+});
+
 //agrego manejador delloginRouter
 app.use('/admin/login', loginRouter)
 
+/* 
+MAS DE VARIABLES DE SESION
+app.get('/', function(req, res) {
+  var conocido = Boolean(req.session.nombre);
 
-//Aca pongo los ejemplos  y distitnas rutas para ir probando. Luego tendre que sacar o modificar seguramente. Esto basado en la intoduccion de Node, segureo despes tenga que cambair esto.
+  res.render('index', { 
+    title: 'Sesiones en Express.js',
+    conocido: conocido,
+    nombre: req.session.nombre
+  });
+  
+  app.post('/ingresar', function(req, res) {
+    if (req.body.nombre) {
+      req.session.nombre = req.body.nombre;
+    }
+
+    res.redirect('/');
+  });
+
+  app.get('/salir', function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+  });
+
+  */
+
+
+
+//Aca pongo los ejemplos  y distitnas rutas para ir probando. Luego tendre que sacar o modificar seguramente. Esto basado en la intoduccion de Node, segureo despes tenga que cambair esto. Esto fue la tarea de Introducciona  Node
 /*
 app.get('/prueba', function(req, res,next) {
   res.send('Hola mundo, estoy probando la ruta');
